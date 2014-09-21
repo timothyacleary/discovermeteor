4b8e23871dcab0c5ca1d36d723656d90ef8545d3
Tim Cleary <timothyacleary@gmail.com>
2014-09-21 17:02:43 -0400

    7-6 use method for submit

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,28 @@
 Posts = new Meteor.Collection('posts');
+
+Meteor.methods({
+	post: function(postAttributes) {
+		var user = Meteor.user(), 
+			postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+		if (!user)
+			throw new Meteor.Error(401, "You need to loging to post new stories");
+
+		if (!postAttributes.title)
+			throw new Meteor.Error(422, 'Please fill in a headline');
+
+		if (postAttributes.url && postWithSameLink) {
+			throw new Meteor.Error(302, 'This link has already been posted'),
+			postWithSameLink._id;
+		}
+
+		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+
+		var postId = Posts.insert(post);
+		return postId;
+	}
+});
